312d3fdd893b23d0000753b4b00d2518ea3ecced
Daniel <daniel.andressanchez12@gmail.com>
2024-09-30 20:36:30 +0000

    Finalizacion ejercicio 56

--- clase30sept2024/Ejercicio56.java
+++ clase30sept2024/Ejercicio56.java
@@ -1,7 +1,27 @@
-public class Ejercicio56{
+import java.util.Scanner;
+
+public class Ejercicio56 {
     public static void main(String[] args) {
-        
-        System.out.println("mi primer programa de JAVA en Github");
+        double nota = 0.0;
+        int aprobados = 0, noAprobados = 0;
+        // Notas de los estudiantes
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Digite la cantidad de estudiantes: ");
+        int estudiantes = leer.nextInt();
+
+        for (int i = 1; i <= estudiantes; i++) {
+            System.out.print("Nota del estudiante " + i + ": ");
+            nota = leer.nextDouble();
+            if (nota >= 5.0) {
+                aprobados++;
+            } else {
+                noAprobados++;
+            }
+
+        }
+
+        System.out.println("Aprobados: " + aprobados + " No Aprobados: " + noAprobados);
 
     }
+
 }
